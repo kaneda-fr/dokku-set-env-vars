92f4eabe66e5713b5786febf69104c5368c238d3
Sebastien Lacoste-Seris <sebastien@lacoste-seris.net>
2020-09-01 11:19:04 +0200

    Fixed execution of dokku commands on remote. dokku user is restricted to launch dokku command on connection.

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,7 @@ ssh-keyscan $DOKKU_HOST >> ~/.ssh/known_hosts
 # Push to Dokku git repository
 SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $DOKKU_USER@$DOKKU_HOST"
 
-$SSH_COMMAND << EOF
-dokku apps:list |grep  $DOKKU_APP_NAME --silent  || dokku apps:create $DOKKU_APP_NAME
-if [[ $(dokku config:get $APP $DOKKU_VAR_NAME) != $DOKKU_VAR_VALUE ]] ; then
-  dokku config:set $APP $DOKKU_VAR_NAME=$DOKKU_VAR_VALUE
+eval $SSH_COMMAND apps:list |grep  $DOKKU_APP_NAME --silent  || eval $SSH_COMMAND apps:create $DOKKU_APP_NAME
+if [[ $(eval $SSH_COMMAND config:get $APP $DOKKU_VAR_NAME) != $DOKKU_VAR_VALUE ]] ; then
+  eval $SSH_COMMAND config:set $APP $DOKKU_VAR_NAME=$DOKKU_VAR_VALUE
 fi
-EOF
